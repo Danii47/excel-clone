1a284da595bb781655d93d547253a485cd3b4cc1
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-08-23 23:31:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# EXCEL CLONE
+
+Excel visual clone with React and TypeScript.
+
+> You can test here: [Excel Clone](https://search-ip-info.vercel.app/)
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+
+const WIDTH = 10
+const HEIGHT = 5
+
+function App() {
+  const [table, setTable] = useState(new Array(HEIGHT).fill(null).map(() => new Array(WIDTH).fill(null)))
+
+  const handleChangeCell = (event: React.ChangeEvent<HTMLInputElement>, i: number, j: number) => {
+    const { value } = event.target
+    const newTable = table.map(row => [...row])
+    newTable[i][j] = value
+    setTable(newTable)
+    console.log(newTable)
+  }
+
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            {
+              new Array(WIDTH).fill(null).map((_, i) => (
+                <th key={i}>{String.fromCharCode("A".charCodeAt(0) + i)}</th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            table.map((row, i) => (
+              <tr key={i}>
+                <td className="rows-keys">{i + 1}</td>
+                {
+                  row.map((cell, j) => (
+                    <td key={j} className="cell">
+                      <input type="text" value={cell ?? ''} onChange={(event) => handleChangeCell(event, i, j)}/>
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
